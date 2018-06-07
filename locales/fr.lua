b396990a6ad400db3b1322bf5bdac0f76d69989c
Cr4zZyBipBiip <Cr4zZyBipBiip@users.noreply.github.com>
2018-06-07 17:14:39 +0200

    Update fr.lua

--- locales/fr.lua
+++ locales/fr.lua
@@ -9,5 +9,11 @@ Locales['fr'] = {
 	['clothes'] = 'vêtements',
 	['you_paid'] = 'vous avez payé $',
 	['save_in_dressing'] = 'voulez-vous donner un nom à votre tenue ?',
+	['buy_new_clothes'] = 'Acheter une nouvelle tenue',
+	['change_clothe'] = 'Parcourir les tenues sauvegardées',
+	['del_clothe'] = 'Supprimer une tenue sauvegardée',
+	['choose_clothe'] = 'Quelle tenue voulez-vous mettre ?',
+	['which_delete'] = 'Quelle tenue voulez-vous supprimer ?',
+	['clothe_delete'] = 'Tenue supprimée',
 
 }
